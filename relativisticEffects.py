51e405ca5cdb8bddb107a3424083838c08462d9f
fetoffah-beep <felixenyimah.toffah@mail.polimi.it>
2021-08-13 12:57:35 +0200

    Restructure the file

--- relativisticEffects.py
+++
@@ -1,7 +0,0 @@
-# The Control Segment will utilize the following alternative but equivalent expression for the
-# relativistic effect when estimating the LNAV/CNAV parameters.
-
-c = 299792458   # m/s
-sv_Pos = 6348893.6251   
-sv_vel = -55726.9751
-delta_tr = -2 * sv_Pos * sv_vel / (c)**2
