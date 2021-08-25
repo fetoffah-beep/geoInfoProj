25d8c31f147ca0566933c911a0b207540e5d273d
ale9620 <alessandro6.gatti@mail.polimi.it>
2021-08-25 18:13:01 +0200

    Update cart2geod.py

--- functions/cart2geod.py
+++ functions/cart2geod.py
@@ -5,24 +5,24 @@ Created on Tue May  4 16:23:59 2021
 @author: 39351
 """
 import math
-import rotationParam
+import functions.rotationParam as rp
 
 
 def cart2geod(x, y, z):
     #Define the eccentricity wrt semi-minor axis
-    e_b = math.sqrt((rotationParam.a**2 - rotationParam.b**2)/(rotationParam.b**2))
+    e_b = math.sqrt((rp.a**2 - rp.b**2)/(rp.b**2))
 
     r = math.sqrt(x**2 + y**2)
 
-    psi  = math.atan(z / (r * math.sqrt(1 - rotationParam.eSquared)))    
+    psi  = math.atan2(z , (r * math.sqrt(1 - rp.eSquared)))    
     
     #Longitude
-    lam = math.atan(y / x)    
+    lam = math.atan2(y , x)    
     
     #Latitude
-    phi  = math.atan((z + e_b**2 * rotationParam.b * pow(math.sin(psi), 3)) / (r - rotationParam.eSquared * rotationParam.a * pow(math.cos(psi), 3)))
+    phi  = math.atan2((z + e_b**2 * rp.b * pow(math.sin(psi), 3)) , (r - rp.eSquared * rp.a * pow(math.cos(psi), 3)))
 
-    Rn = rotationParam.a / (math.sqrt(1 - rotationParam.eSquared * pow(math.sin(phi), 2)))
+    Rn = rp.a / (math.sqrt(1 - rp.eSquared * pow(math.sin(phi), 2)))
 
     #Geodetic height
     h = (r / math.cos(phi)) - Rn
